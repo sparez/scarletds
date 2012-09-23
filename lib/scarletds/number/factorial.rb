1655943c179e554aa42dc2d6638c887ac6483eac
Reza Marvan Spagnolo <marvans@gmail.com>
2012-09-23 19:20:42 +0200

    moved opening of Integer class inside test class of factorial referencing explicitly global namespace

--- lib/scarletds/number/factorial.rb
+++ lib/scarletds/number/factorial.rb
@@ -1,16 +1,13 @@
+
 module Rubyds
   module Number
-
     # mixin for Integer class
     module Factorial
-
       # return factorial
       def factorial
         return 1 if self == 0
         1.upto(self).inject(:*)
       end
-
     end
-
   end
 end
--- test/scarletds/number/tc_factorial.rb
+++ test/scarletds/number/tc_factorial.rb
@@ -2,11 +2,11 @@
 require 'test/unit'
 require 'scarletds/number/factorial'
 
-class Integer
-  include Rubyds::Number::Factorial
-end
-
 class TestFactorial < Test::Unit::TestCase
+
+  class ::Integer
+    include Rubyds::Number::Factorial
+  end
 
   def test_factorial
     assert_equal 120, 5.factorial
